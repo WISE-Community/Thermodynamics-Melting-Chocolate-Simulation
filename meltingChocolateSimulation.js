179a58e8c801f64e1b0410b426ed9cbfc7c3f72b
Geoffrey Kwan <geoffreykwan@gmail.com>
2017-11-16 16:06:27 -0800

    Added seconds timers for the chocolates.

--- meltingChocolateSimulation.js
+++ meltingChocolateSimulation.js
@@ -10,7 +10,6 @@ var selection2 = null;
 var startButtonBool = false;
 var resetButtonBool = false;
 var drawArrow2Bool = false;
-var set = null;
 
 var metal = null;
 var metalText = null;
@@ -35,6 +34,9 @@ var animationMaterial1Y = 30;
 
 var selectBarSecondYSpacing = 200;
 
+var firstChocolateTimer = 0;
+var secondChocolateTimer = 0;
+
 function init() {
   draw = SVG('board');
   arr = [];
@@ -45,13 +47,15 @@ function init() {
   startButtonBool = false;
   resetButtonBool = false;
   drawArrow2Bool = false;
-  set = draw.set();
 
   selectBarX = 180;
   selectBarY = 80;
   selectBarYSpacing = 30;
 
   textXSpacing = 20;
+
+  drawStartButton();
+  drawResetButton();
 
 
   <!-- ----------------GROUP 1-------------------- -->
@@ -81,6 +85,7 @@ function init() {
   woodText = draw.text('Wood').x(selectBarX + selectMaterialWidth + textXSpacing).y(selectBarY + (selectBarYSpacing * 2));
 
   drawArrow1();
+  drawFirstChocolateTimer();
   <!-- ------------------------------------------- -->
 
   <!-- ----------------GROUP 2-------------------- -->
@@ -110,6 +115,8 @@ function init() {
     selectSecondMaterial('wood')
   }).addClass('clickable');
   woodText2 = draw.text('Wood').x(selectBarX + selectMaterialWidth + textXSpacing).y(selectBarY + selectBarSecondYSpacing + (selectBarYSpacing * 2));
+
+  drawSecondChocolateTimer();
   <!-- ------------------------------------------- -->
 }
 
@@ -122,10 +129,10 @@ function selectFirstMaterial(material) {
     iron.remove();
   }
   //resets if the start button has been pressed
-  if (startButtonBool === true) {
-    drawStartButton();
-    stopAnim();
-  }
+  // if (startButtonBool === true) {
+  //   drawStartButton();
+  //   stopAnim();
+  // }
   arrow.remove()
   arrowText.remove();
   click1 = true;
@@ -134,13 +141,12 @@ function selectFirstMaterial(material) {
     'x': animationMaterialX,
     'y': animationMaterial1Y
   })
-  set.add(plank);
   arr.push(plank);
   iron = draw.image('./img/iron.png', 50, 50).attr({
     'x': 15,
     'y': 30
     });
-  drawArrow2()
+  drawArrow2();
 }
 
 function setSelection1(material) {
@@ -188,14 +194,12 @@ function selectSecondMaterial(material) {
   }
   arrow2.remove()
   arrowText2.remove();
-  drawStartButton();
-  drawResetButton();
+  showStartButton();
   setSelection2(material);
   plank2 = draw.image('./img/' + material + '.png', animationMaterialWidth, animationMaterialHeight).attr({
     'x': animationMaterialX,
     'y': animationMaterial1Y + selectBarSecondYSpacing
   })
-  set.add(plank2);
   arr.push(plank2);
   iron2 = draw.image('./img/iron.png', 50, 50).attr({
   'x': 15,
@@ -224,23 +228,27 @@ function heatAnim() {
   //heat animation for top material, runs chocolate animation after
   rect1.animate(selection1 * 3000 + 3000, 'quadIn').attr({
     'width': 400
-  }).after(poopCall1);
+  }).after(animateFirstChocolateMelting).duringAll((pos) => {
+    updateFirstChocolateTimer();
+  });
   //heat animation for bottom material, runs chocolate animation after
   rect2.animate(selection2 * 3000 + 3000, 'quadIn').attr({
     'width': 400
-  }).after(poopCall2);
+  }).after(animateSecondChocolateMelting).duringAll((pos) => {
+    updateSecondChocolateTimer();
+  });
 }
 
-function poopAnim(num, bool) {
+function poopAnim0(num, bool) {
   if (num === 1) {
-    chip.animate(2000).opacity(0);
+    firstMaterialChocolateNotMelted.animate(2000).opacity(0);
     setTimeout(function() {
-      chip2.animate(1000).opacity(1).after(function() {
-        chip2.animate(1500).opacity(0);
+      firstMaterialChocolateHalfMelted.animate(1000).opacity(1).after(function() {
+        firstMaterialChocolateHalfMelted.animate(1500).opacity(0);
       })
     }, 1000);
     setTimeout(function() {
-      chip3.animate(1000).opacity(1);
+      firstMaterialChocolateMelted.animate(1000).opacity(1);
     }, 2600);
     if (bool) {
       setTimeout(function() {
@@ -254,14 +262,14 @@ function poopAnim(num, bool) {
       }, 4000);
     }
   } else {
-    chip1.animate(2000).opacity(0);
+    secondMaterialChocolateNotMelted.animate(2000).opacity(0);
     setTimeout(function() {
-      chip4.animate(1000).opacity(1).after(function() {
-        chip4.animate(1500).opacity(0);
+      secondMaterialChocolateHalfMelted.animate(1000).opacity(1).after(function() {
+        secondMaterialChocolateHalfMelted.animate(1500).opacity(0);
       })
     }, 1000);
     setTimeout(function() {
-      chip5.animate(1000).opacity(1);
+      secondMaterialChocolateMelted.animate(1000).opacity(1);
     }, 2600);
     if (bool) {
       setTimeout(function() {
@@ -278,8 +286,14 @@ function poopAnim(num, bool) {
 }
 
 function stopAnim() {
-  var chips = [chip, chip1, chip2, chip3, chip4, chip5, rect1, rect2];
-  // set.stop();
+  var chips = [rect1,
+      rect2,
+      firstMaterialChocolateNotMelted,
+      firstMaterialChocolateHalfMelted,
+      firstMaterialChocolateMelted,
+      secondMaterialChocolateNotMelted,
+      secondMaterialChocolateHalfMelted,
+      secondMaterialChocolateMelted];
   for (var i = 0 ; i < chips.length; i++) {
     chips[i].remove();
   }
@@ -300,8 +314,8 @@ function reset() {
 
 function drawStartButton() {
   if (startButtonBool === false) {
-    startButtonText = draw.text('Start').x(478).y(138).font({size: 25});
-    startButtonIcon = draw.rect(150,50).x(430).y(130).radius(15).fill('white').stroke({width:2}).opacity(1).attr({
+    startButtonText = draw.text('Start').x(458).y(178).font({size: 25});
+    startButtonIcon = draw.rect(150,50).x(410).y(170).radius(15).fill('white').stroke({width:2}).opacity(1).attr({
       'fill-opacity': 0
     }).addClass('clickable');
     arr.push(startButtonIcon, startButtonText);
@@ -311,13 +325,27 @@ function drawStartButton() {
     submission[2] = new Date().toLocaleString();
     console.log(submission);
     startAnim();
+    hideStartButton();
+    showResetButton();
   })
+
+  hideStartButton();
+}
+
+function showStartButton() {
+  startButtonText.show();
+  startButtonIcon.show();
+}
+
+function hideStartButton() {
+  startButtonText.hide();
+  startButtonIcon.hide();
 }
 
 function drawResetButton() {
   if (resetButtonBool === false) {
-    resetButtonText = draw.text('Reset').x(473).y(248).font({size: 25});
-    resetButtonIcon = draw.rect(150,50).x(430).y(240).radius(15).fill('white').stroke({width:2}).opacity(1).attr({
+    resetButtonText = draw.text('Reset').x(453).y(178).font({size: 25});
+    resetButtonIcon = draw.rect(150,50).x(410).y(170).radius(15).fill('white').stroke({width:2}).opacity(1).attr({
       'fill-opacity': 0
     }).addClass('clickable');
     resetButtonBool = true;
@@ -326,6 +354,18 @@ function drawResetButton() {
   resetButtonIcon.click(function() {
     reset();
   })
+
+  hideResetButton();
+}
+
+function showResetButton() {
+  resetButtonText.show();
+  resetButtonIcon.show();
+}
+
+function hideResetButton() {
+  resetButtonText.hide();
+  resetButtonIcon.hide();
 }
 
 function drawArrow1() {
@@ -415,53 +455,117 @@ function drawHeat() {
   let chocolateYOffset = 10;
   let chocolateXOffset = animationMaterialWidth - 100;
 
-  chip = draw.image('./img/chip1.png', 128, 50).attr({
+  firstMaterialChocolateNotMelted = draw.image('./img/chip1.png', 128, 50).attr({
     'x': animationMaterialX + chocolateXOffset,
     'y': animationMaterial1Y - chocolateYOffset
   });
 
-  chip1 = draw.image('./img/chip1.png', 128, 50).attr({
+  firstMaterialChocolateHalfMelted = draw.image('./img/chip2.png', 128, 50).attr({
+    'x': animationMaterialX + chocolateXOffset,
+    'y': animationMaterial1Y - chocolateYOffset,
+    'opacity': 0
+  });
+
+  firstMaterialChocolateMelted = draw.image('./img/chip3.png', 128, 50).attr({
+    'x': animationMaterialX + chocolateXOffset,
+    'y': animationMaterial1Y - chocolateYOffset,
+    'opacity': 0
+  });
+
+  secondMaterialChocolateNotMelted = draw.image('./img/chip1.png', 128, 50).attr({
     'x': animationMaterialX + chocolateXOffset,
     'y': animationMaterial1Y + selectBarSecondYSpacing - chocolateYOffset
   });
 
-  chip2 = draw.image('./img/chip2.png', 128, 50).attr({
-    'x': animationMaterialX + chocolateXOffset,
-    'y': animationMaterial1Y - chocolateYOffset,
-    'opacity': 0
-  });
-  chip3 = draw.image('./img/chip3.png', 128, 50).attr({
-    'x': animationMaterialX + chocolateXOffset,
-    'y': animationMaterial1Y - chocolateYOffset,
-    'opacity': 0
-  });
-  chip4 = draw.image('./img/chip2.png', 128, 50).attr({
+  secondMaterialChocolateHalfMelted = draw.image('./img/chip2.png', 128, 50).attr({
     'x': animationMaterialX + chocolateXOffset,
     'y': animationMaterial1Y + selectBarSecondYSpacing - chocolateYOffset,
     'opacity': 0
   });
-  chip5 = draw.image('./img/chip3.png', 128, 50).attr({
+
+  secondMaterialChocolateMelted = draw.image('./img/chip3.png', 128, 50).attr({
     'x': animationMaterialX + chocolateXOffset,
     'y': animationMaterial1Y + selectBarSecondYSpacing - chocolateYOffset,
     'opacity': 0
   });
-  set.add(rect1, rect2, chip, chip1, chip2, chip3, chip4, chip5);
-  arr.push(rect1, rect2, chip, chip1, chip2, chip3, chip4, chip5);
+  arr.push(rect1,
+      rect2,
+      firstMaterialChocolateNotMelted,
+      firstMaterialChocolateHalfMelted,
+      firstMaterialChocolateMelted,
+      secondMaterialChocolateNotMelted,
+      secondMaterialChocolateHalfMelted,
+      secondMaterialChocolateMelted);
 }
 
-function poopCall1() {
-  var bool = maxIndex === 0;
-  poopAnim(1, bool);;
+function animateFirstChocolateMelting() {
+  //var bool = maxIndex === 0;
+  //poopAnim(1, bool);
   // if (maxIndex === 0) {
   //   drawStartButton();
   // }
+
+  firstMaterialChocolateNotMelted.animate(1000).opacity(0.5).after(() => {
+    firstMaterialChocolateNotMelted.animate(1000).opacity(0);
+    firstMaterialChocolateHalfMelted.animate(1000).opacity(1).after(() => {
+      firstMaterialChocolateHalfMelted.animate(1000).opacity(0);
+      firstMaterialChocolateMelted.animate(1000).opacity(1);
+    });
+  });
+  // firstMaterialChocolateNotMelted.animate(2000).opacity(0);
+  // setTimeout(function() {
+  //   firstMaterialChocolateHalfMelted.animate(1000).opacity(1).after(function() {
+  //     firstMaterialChocolateHalfMelted.animate(1500).opacity(0);
+  //   })
+  // }, 1000);
+  // setTimeout(function() {
+  //   firstMaterialChocolateMelted.animate(1000).opacity(1);
+  // }, 2600);
+  // if (bool) {
+  //   setTimeout(function() {
+  //     startButtonIcon.click(function() {
+  //       startButtonBool = true;
+  //       submission[2] = new Date().toLocaleString();
+  //       console.log(submission);
+  //       log();
+  //       startAnim();
+  //     })
+  //   }, 4000);
+  // }
 }
 
-function poopCall2() {
-  var bool = maxIndex === 1;
-  poopAnim(2, bool);
+function animateSecondChocolateMelting() {
+  //var bool = maxIndex === 1;
+  //poopAnim(2, bool);
   // if (maxIndex === 1) {
   //   drawStartButton();
+  // }
+
+  secondMaterialChocolateNotMelted.animate(1000).opacity(0.5).after(() => {
+    secondMaterialChocolateNotMelted.animate(1000).opacity(0);
+    secondMaterialChocolateHalfMelted.animate(1000).opacity(1).after(() => {
+      secondMaterialChocolateHalfMelted.animate(1000).opacity(0);
+      secondMaterialChocolateMelted.animate(1000).opacity(1);
+    });
+  });
+  // setTimeout(function() {
+  //   secondMaterialChocolateHalfMelted.animate(1000).opacity(1).after(function() {
+  //     secondMaterialChocolateHalfMelted.animate(1500).opacity(0);
+  //   })
+  // }, 1000);
+  // setTimeout(function() {
+  //   secondMaterialChocolateMelted.animate(1000).opacity(1);
+  // }, 2600);
+  // if (bool) {
+  //   setTimeout(function() {
+  //     startButtonIcon.click(function() {
+  //       startButtonBool = true;
+  //       submission[2] = new Date().toLocaleString();
+  //       console.log(submission);
+  //       log();
+  //       startAnim();
+  //     })
+  //   }, 4000);
   // }
 }
 
@@ -471,6 +575,40 @@ function removeChips() {
       chips[i].stop();
       chips[i].remove();
   }
+}
+
+function startFirstChocolateTimer() {
+
+}
+
+function startSecondChocolateTimer() {
+
+}
+
+function startFirstChocolateTimer() {
+
+}
+
+function startSecondChocolateTimer() {
+
+}
+
+function drawFirstChocolateTimer() {
+  firstChocolateTimerText = draw.text(firstChocolateTimer + ' seconds').x(animationMaterialX + animationMaterialWidth + 50).y(animationMaterial1Y + 10);
+}
+
+function drawSecondChocolateTimer() {
+  secondChocolateTimerText = draw.text(secondChocolateTimer + ' seconds').x(animationMaterialX + animationMaterialWidth + 50).y(selectBarSecondYSpacing + animationMaterial1Y + 10);
+}
+
+function updateFirstChocolateTimer() {
+  firstChocolateTimer += 1;
+  firstChocolateTimerText.text(firstChocolateTimer + ' seconds');
+}
+
+function updateSecondChocolateTimer() {
+  secondChocolateTimer += 1;
+  secondChocolateTimerText.text(secondChocolateTimer + ' seconds');
 }
 
 function log() {
